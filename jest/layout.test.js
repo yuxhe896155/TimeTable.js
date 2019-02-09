0f9332732eb723c2d4048ab291793740707bf918
yn1323 <yn1323@gmail.com>
2019-02-10 01:44:42 +0900

    Delete files

--- jest/layout.test.js
+++
@@ -1,5 +0,0 @@
-let t = require("../modules/js/layout").default;
-let ins = new t();
-test('adds 1 + 2 to equal 3', () => {
-    expect(ins.log(5)).toBe(6);
-});
--- modules/js/common/common.js
+++
@@ -1,6 +0,0 @@
-class common{
-    static log(a){
-        return a + 1;
-    }
-}
-exports.default = common;
