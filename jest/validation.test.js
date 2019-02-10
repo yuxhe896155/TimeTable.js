c814014d52bc539d3c02f128e1d7d83359b6dd1b
yn1323 <yn1323@gmail.com>
2019-02-11 01:10:41 +0900

    Added test

--- jest/validation.test.js
+++ jest/validation.test.js
@@ -21,4 +21,25 @@ describe("validation.checkSelector()", ()=>{
             expect(v.checkSelector(val.param)).toBe(val.expect);
         }
     });
+    test("html existance check", ()=>{
+        document.body.innerHTML = "<div class=\"prural\"><div class=\"prural\"></div></div>";
+        let plan = [
+            {param: "#target",      expect: false},
+            {param: ".target",       expect: false},
+            {param: ".prural",       expect: false},
+        ];
+        for(let val of plan){
+            expect(v.checkSelector(val.param)).toBe(val.expect);
+        }
+    });
+    test("Normal test", ()=>{
+        document.body.innerHTML = "<div id=\"target\" class=\"target\"></div>";
+        let plan = [
+            {param: "#target",      expect: true},
+            {param: ".target",       expect: true},
+        ];
+        for(let val of plan){
+            expect(v.checkSelector(val.param)).toBe(val.expect);
+        }
+    });
 });
